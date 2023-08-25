9e5ba3d0f3230fadde35a9d54d52d12fa72a1ded
sahil <sahiltyagi1204@outlook.com>
2023-08-25 22:48:05 +0530

    add functions to connect and disconnect user

--- public/util/users.js
+++ public/util/users.js
@@ -15,7 +15,24 @@ function getCurrentUser(id){
     return users.find(user => user.id === id)
 }
 
+// user leaves
+function userLeave(id){
+    const index = users.findIndex(user =>user.id === id)
+
+    if(index !== -1){
+        return users.splice(index , 1)[0]
+    }
+}
+
+// get room users
+
+function getRoomUsers(room){
+    return users.filter(user => user.room === room)
+}
+
 module.exports ={
     userJoin,
-    getCurrentUser
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
 }
