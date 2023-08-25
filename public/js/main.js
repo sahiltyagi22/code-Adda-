dab9c6a0fee27936e984cc7906b5856ef6193f3c
sahil <sahiltyagi1204@outlook.com>
2023-08-25 22:46:52 +0530

    added users add and remove functinality and rooms

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
 
 const {username , room} = Qs.parse(location.search , {
     ignoreQueryPrefix : true
@@ -12,6 +14,13 @@ const socket = io()
 
 // join chat room
 socket.emit('joinRoom' , {username , room})
+
+// get room and users
+
+socket.on('roomUsers' , ({room , users}) =>{
+    outputRoomName(room)
+    outputUsers(users)
+})
 
 
 //  message from server
@@ -49,3 +58,20 @@ function outputMessage(message){
     document.querySelector('.chat-messages').appendChild(div)
 
 }
+
+// add room name to DOM
+
+function outputRoomName(room){
+    roomName.innerText = room
+}
+
+//  add users to DOM
+
+function outputUsers(users){
+    userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+    
+    
+    `
+}
+
